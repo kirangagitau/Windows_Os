7044acb57c2faaec5fc66510c8b6a73015b95e1b
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-14 17:22:27 +0300

    Update create_hotspot.bat

--- Networking/create_hotspot.bat
+++ Networking/create_hotspot.bat
@@ -1,1 +1,13 @@
+#THIS SCRIPT SHOWS HOW TO START A LOCAL HOSTED NETWORK FROM WINDOWS MACHINE 
+echo '@Author Gitau Muguro'
 
+#View if wireless drivers are active
+netsh wlan show drivers 
+
+#configure/setup the hostednetwork
+set hostednetwork mode =allow
+set hostednetwork mode ssid="My network"
+set hostednetwork key='passwd' keyUsade=temporary
+
+#now start the hostednetwork
+start hostednetwork
