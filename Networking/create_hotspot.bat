406352fdba77e4e4d89a2cdbca5847388d492aea
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-14 17:27:21 +0300

    Create Hot-Spot

--- Networking/create_hotspot.bat
+++ Networking/create_hotspot.bat
@@ -1,13 +1,17 @@
-#THIS SCRIPT SHOWS HOW TO START A LOCAL HOSTED NETWORK FROM WINDOWS MACHINE 
+echo off
+:: THIS SCRIPT SHOWS HOW TO START A LOCAL HOSTED NETWORK FROM WINDOWS MACHINE 
 echo '@Author Gitau Muguro'
 
-#View if wireless drivers are active
+:: View if wireless drivers are active
 netsh wlan show drivers 
 
-#configure/setup the hostednetwork
+:: configure/setup the hostednetwork
 set hostednetwork mode =allow
 set hostednetwork mode ssid="My network"
 set hostednetwork key='passwd' keyUsade=temporary
 
-#now start the hostednetwork
+:: now start the hostednetwork
 start hostednetwork
+
+:: once done using the network, you can turn it off by following command
+:: netsh wlan stop hostednetwork
