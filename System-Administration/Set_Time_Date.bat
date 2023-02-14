0fe8282eef6eaaee51ed316d13ef4464a3908363
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-14 17:59:02 +0300

    Set Time

---
+++ System-Administration/Set_Time_Date.bat
@@ -0,0 +1,11 @@
+:: This batch file is used to set the date and time
+@echo off 
+echo '@Author Gitau Muguro'
+
+:: set date
+:: Date in system is displayed, if correct, hit enter to skip changing it.
+date
+
+::set Time
+:: Current time of system is displayed, if correct, hit enter to skip changing it.
+time
