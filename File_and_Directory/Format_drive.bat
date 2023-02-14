d3dc5b442d0159dd8087f7134be4ab752cb230e3
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-14 17:42:40 +0300

    Update Format_drive.bat

--- File_and_Directory/Format_drive.bat
+++ File_and_Directory/Format_drive.bat
@@ -1,8 +1,12 @@
-#This script guides in formating a disk using the CLI
-
+:: This script guides in formating a disk using the CLI
+echo off
 echo '@Author Gitau Muguro'
 
 disk part
-list volume  # or select disk
-select volume x #or select disk x
+list volume  
+:: or use 'list disk'
+
+select volume x
+:: or use 'select disk x' 
+:: where x is the disk or volume to be operated on.  
 FORMAT FS=NTFS LABEL="New Volume" QUICK COMPRESS
