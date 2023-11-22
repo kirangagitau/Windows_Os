47b6880901d171d9089ed88dd6b2ed975a3c5610
Gitau Muguro <dennosea@gmail.com>
2023-11-22 21:06:26 +0300

    Create Reboot_Windows_into_bios.bat

---
+++ Reboot_Windows_into_bios.bat
@@ -0,0 +1,6 @@
+REM this command boots a windows computer into bios. 
+:: Open CMD (prefereably as ADMIN, and run command below)
+shutdown /r /fw
+
+:: However it may fail in some computers.
+:: Hence consider other options.
